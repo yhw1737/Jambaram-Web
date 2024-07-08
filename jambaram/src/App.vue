4f657da0d0f5ea926d9a5dbfd5c38c07a88c34e6
yhw1737 <yhw1737@naver.com>
2024-07-08 12:47:50 +0900

    데이터바인딩+v-for 사용법 익힘

--- jambaram/src/App.vue
+++ jambaram/src/App.vue
@@ -1,13 +1,11 @@
 <template>
 <div>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div>
-    <h4>XX 원룸</h4>
-    <p>{{ price1 }} 만원</p>
+  <div class="menu">
+    <a v-for="i in 메뉴" :key="i">{{ i }}</a>
   </div>
-  <div>
-    <h4>XX 원룸</h4>
-    <p>{{ price2 }} 만원</p>
+  <div v-for="(a,i) in products" :key="i">
+    <h4>{{ a }}</h4>
+    <p>{{ prices[i] }} 만원</p>
   </div>
 </div>
 </template>
@@ -18,8 +16,9 @@ export default {
   name: 'App',
   data(){
     return{
-      price1 : 60,
-      price2 : 70,
+      prices : [60, 70, 80],
+      메뉴 : ['Home', 'Shop', 'About'],
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
   },
   components: {
@@ -36,4 +35,15 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.menu{
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a{
+  color: white;
+  padding: 10px;
+}
+
 </style>
