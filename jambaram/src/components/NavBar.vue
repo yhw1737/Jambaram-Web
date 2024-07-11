5258fe67d536bc021d4eb0442cf26759d143b7be
yhw1737 <yhw1737@naver.com>
2024-07-11 12:28:34 +0900

    ad banner, menu, img control

--- jambaram/src/components/NavBar.vue
+++ jambaram/src/components/NavBar.vue
@@ -1,26 +1,53 @@
 <template>
-    <div class="menu">
-      <router-link to="/">Home</router-link>
-      <router-link to="/champions">Champions</router-link>
-      <router-link to="/stats">Stats</router-link>
-    </div>
+    <nav class="nav-bar">
+      <ul class="nav-list">
+        <li v-for="item in menuItems" :key="item.name" class="nav-item">
+          <router-link :to="item.path">{{ item.name }}</router-link>
+        </li>
+      </ul>
+    </nav>
   </template>
   
   <script>
   export default {
-    name: 'NavBar'
+    name: 'NavBar',
+    data() {
+      return {
+        menuItems: [
+          { name: 'Home', path: '/' },
+          { name: 'Champions', path: '/champions' },
+          { name: 'Stats', path: '/stats' }
+        ]
+      };
+    }
   }
   </script>
   
   <style>
-  .menu {
-    background: darkslateblue;
+  .nav-bar {
+    width: 100%;
+    background-color: darkslateblue;
     padding: 15px;
-    border-radius: 5px;
+    box-sizing: border-box;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1000;
   }
-  .menu a {
+  
+  .nav-list {
+    display: flex;
+    justify-content: space-around;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  
+  .nav-item a {
     color: white;
     padding: 10px;
+    text-decoration: none;
   }
   </style>
   
--- jambaram/src/views/HomeView.vue
+++ jambaram/src/views/HomeView.vue
@@ -1,17 +1,27 @@
 <template>
-    <div>
-      <img src="../assets/칼바람.jpg" class="HomeImg">
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'HomeView'
-  }
-  </script>
-  
-  <style>
-  .HomeImg{
-    width: 100%;
-  }
-  </style>
+  <div class="home-container">
+    <img src="../assets/칼바람.jpg" class="home-img">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+}
+</script>
+
+<style>
+.home-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.home-img {
+  width: 100%;
+  height: auto;
+  max-height: 400px; /* 세로 길이를 제한하여 가로로 넓게 보이도록 설정 */
+  object-fit: cover;
+}
+</style>
