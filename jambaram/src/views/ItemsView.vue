44379fe07a9662380c5deb5d06345f4c5dcabac1
yhw1737 <yhw1737@naver.com>
2024-07-20 22:30:30 +0900

    아이템받아오기

--- jambaram/src/views/ItemsView.vue
+++ jambaram/src/views/ItemsView.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>
-    <input type="text" v-model="searchQuery" placeholder="아이템 검색" class="search-bar">
+  <div class="items-container">
+    <div class="left-menu">
+      <div @click="setMap('all')" :class="{ active: selectedMap === 'all' }">전체</div>
+      <div @click="setMap(11)" :class="{ active: selectedMap === 11 }">소환사의 협곡</div>
+      <div @click="setMap(12)" :class="{ active: selectedMap === 12 }">칼바람 나락</div>
+      <div @click="setMap('other')" :class="{ active: selectedMap === 'other' }">기타</div>
+    </div>
+    <div class="right-menu">
+      <div @click="setTag('all')" :class="{ active: selectedTag === 'all' }">전체</div>
+      <div v-for="tag in tags" :key="tag" @click="setTag(tag)" :class="{ active: selectedTag === tag }">{{ tag }}</div>
+    </div>
     <div class="item-list">
-      <div v-for="(item, index) in filteredItems" :key="index" class="item">
-        <img :src="`http://ddragon.leagueoflegends.com/cdn/${gameversion}/img/item/${item.image.full}`" alt="item" class="item-img">
-        <div class="item-name">{{ item.koreanName }}</div>
+      <div v-for="item in filteredItems" :key="item.id" class="item">
+        <img :src="`http://ddragon.leagueoflegends.com/cdn/${gameversion}/img/item/${item.image.full}`" :alt="item.name" />
+        <div>{{ item.name }}</div>
       </div>
     </div>
   </div>
@@ -17,58 +26,76 @@ export default {
   name: 'ItemsView',
   data() {
     return {
-      gameversion: "14.14.1",
-      searchQuery: '',
+      gameversion: '14.14.1',
       items: [],
-      loading: true,
+      selectedMap: 'all',
+      selectedTag: 'all',
+      tags: ['Attack', 'Defense', 'Magic', 'Movement', 'Consumable', 'Jungle', 'Vision', 'GoldPer', 'CooldownReduction', 'CriticalStrike', 'OnHit', 'Mana'],
     };
   },
   computed: {
     filteredItems() {
-      return this.items.filter(item =>
-        item.englishName.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-        item.koreanName.toLowerCase().includes(this.searchQuery.toLowerCase())
-      );
+      return this.items.filter(item => {
+        const mapCondition = this.selectedMap === 'all' || item.maps[this.selectedMap] || (this.selectedMap === 'other' && !item.maps[11] && !item.maps[12]);
+        const tagCondition = this.selectedTag === 'all' || item.tags.includes(this.selectedTag);
+        return mapCondition && tagCondition;
+      });
     }
   },
-  async mounted() {
-    try {
-      const response = await axios.get(`http://ddragon.leagueoflegends.com/cdn/${this.gameversion}/data/ko_KR/item.json`);
-      const koreanData = response.data.data;
-
-      const englishResponse = await axios.get(`http://ddragon.leagueoflegends.com/cdn/${this.gameversion}/data/en_US/item.json`);
-      const englishData = englishResponse.data.data;
-
-      this.items = Object.values(koreanData).map(item => ({
-        ...item,
-        koreanName: item.name,
-        englishName: englishData[item.id].name, // 영어 이름 추가
-        image: { full: item.image.full }
-      }));
-    } catch (error) {
-      console.error('Failed to load item data:', error);
-    } finally {
-      this.loading = false;
+  methods: {
+    setMap(map) {
+      this.selectedMap = map;
+    },
+    setTag(tag) {
+      this.selectedTag = tag;
+    },
+    async fetchItems() {
+      try {
+        const response = await axios.get('http://ddragon.leagueoflegends.com/cdn/14.14.1/data/ko_KR/item.json');
+        this.items = Object.values(response.data.data);
+      } catch (error) {
+        console.error('Failed to fetch items:', error);
+      }
     }
+  },
+  mounted() {
+    this.fetchItems();
   }
-};
+}
 </script>
 
 <style scoped>
-.search-bar {
-  width: calc(100% - 20px);
+.items-container {
+  display: flex;
+  width: 100%;
+  height: 100vh;
+}
+
+.left-menu, .right-menu {
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  background-color: #2c3e50;
+  color: white;
   padding: 10px;
-  font-size: 16px;
-  margin-bottom: 20px;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
+}
+
+.left-menu div, .right-menu div {
+  padding: 10px;
+  cursor: pointer;
+}
+
+.left-menu div.active, .right-menu div.active {
+  background-color: #34495e;
 }
 
 .item-list {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+  padding: 20px;
+  flex-grow: 1;
+  overflow-y: scroll;
 }
 
 .item {
@@ -77,12 +104,8 @@ export default {
   text-align: center;
 }
 
-.item-img {
-  width: 100%;
-}
-
-.item-name {
-  margin-top: 5px;
-  font-size: 14px;
+.item img {
+  width: 48px;
+  height: 48px;
 }
 </style>
